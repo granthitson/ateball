fb4f2808c1922331337b7e35c52eadb4021e2bda
Grant Hitson <30513866+granthitson@users.noreply.github.com>
2023-04-10 21:30:37 -0400

    temp ball identification

--- ateball-py/ball.py
+++ ateball-py/ball.py
@@ -16,8 +16,17 @@ class Ball(Point):
         self.number = int(name[0]) if name is not None else None
         self.target = target
 
-        self.rgb = (0, 255, 0)
-        self.mask = None
+        self.bgr = (0, 255, 0)
+
+        self.ball_mask = None
+        self.white_mask = None
+        self.stick_mask = None
+        self.glove_mask = None
+
+        self.white_total = None
+        self.color_total = None
+        self.ratio = None
+
         self.pocketed = False
 
         self.offset_center = (0, 0)
@@ -27,10 +36,25 @@ class Ball(Point):
         self.logger = logging.getLogger("ateball.ball")
 
     def __str__(self):
-        return self.name
+        return f"Ball({self.name})"
 
     def __repr__(self):
-        return str(self)
+        return f"Ball({self.name})"
+
+    def __hash__(self):
+        return hash(self.__repr__())
+
+    def __eq__(self, other):
+        if isinstance(other, Ball):
+            dist = self.distance(other)
+            if dist:
+                self.logger.debug(f"{self} - {other} : {dist}")
+            return (dist < 5)
+        else:
+            return False
+
+    def __ne__(self, other):
+        return (not self.__eq__(other))
 
     def __copy__(self):
         return Ball(self.center, self.suit, self.name, self.color, self.target)
@@ -39,20 +63,50 @@ class Ball(Point):
         self.center = (center[0], center[1])
         self.offset_center = (center[0] + constants.regions.table[0], center[1] + constants.regions.table[1])
 
+    def get_features(self):
+        # not happy with this process - works most of the time, but want more reliable
+
+        # compensate for stick blocking
+        stick_total = np.sum(np.count_nonzero(self.stick_mask, axis=-1))
+        if stick_total > 9:
+            solid_white_total =  80 - (stick_total * .33)
+            solid_color_total =  230 - (stick_total * .66)
+        else:
+            solid_white_total =  80 - stick_total
+            solid_color_total =  230
+
+        # generate avg ratio comparing total white pixels and colored pixels against their max totals of solids
+        self.white_total = np.sum(np.count_nonzero(self.white_mask, axis=-1))
+        self.color_total = np.sum(np.count_nonzero(self.ball_mask, axis=-1)) / 3
+        self.ratio = ((self.white_total / solid_white_total) + (self.color_total / solid_color_total)) / 2
+
+    def set_identity(self, data):
+        self.suit = data.suit if "suit" in data.__dict__ else None
+        self.name = data.name
+        self.number = data.number if "number" in data.__dict__ else None
+        self.color = data.color if "color" in data.__dict__ else None
+        self.bgr = constants.table.balls.colors.__dict__[self.color].bgr if "color" in data.__dict__ else (0, 255, 0)
+
     def draw(self, image):
-        cv2.circle(image, self.center, 10, self.rgb, 1)
+        if self.suit == "solid":
+            cv2.circle(image, self.center, 8, (self.bgr[0], self.bgr[1], self.bgr[2]), 2)
+        elif self.suit == "stripe":
+            cv2.line(image, (self.center[0] - 8, self.center[1]), (self.center[0] + 8, self.center[1]), (255, 255, 255), 2)
+            cv2.circle(image, self.center, 8, (self.bgr[0], self.bgr[1], self.bgr[2]), 2)
+        else:
+            cv2.circle(image, self.center, 8, (self.bgr[0], self.bgr[1], self.bgr[2]), 2)
 
 class Cue(Ball):
     def __init__(self, center):
         super().__init__(center, None, "cueball", "white", False)
 
-        self.rgb = (255, 255, 255)
+        self.bgr = (255, 255, 255)
         self.number = None
 
 class Eight(Ball):
     def __init__(self, center):
         super().__init__(center, None, "eightball", "black", False)
 
-        self.rgb = (0, 0, 0)
+        self.bgr = (0, 0, 0)
         self.number = 8
 
--- ateball-py/table.py
+++ ateball-py/table.py
@@ -20,6 +20,12 @@ class Table(object):
         self.table_background_mask = np.array(constants.gamemodes.__dict__[gamemode].table_mask_lower), np.array(constants.gamemodes.__dict__[gamemode].table_mask_upper)
         self.table_background_black_mask = np.array(constants.table.black_mask.lower), np.array(constants.table.black_mask.upper)
 
+        self.stick_mask = np.array(constants.table.stick_mask.lower), np.array(constants.table.stick_mask.upper)
+        self.glove_mask = np.array(constants.table.glove_mask.lower), np.array(constants.table.glove_mask.upper)
+        self.white_mask = np.array(constants.table.white_mask.lower), np.array(constants.table.white_mask.upper)
+
+        self.ball_color_look_up = {i : c.match_bgr for i, c in constants.table.balls.colors.__dict__.items()}
+
         self.balls = []
         self.updated = threading.Event()
 
@@ -71,9 +77,154 @@ class Table(object):
         points = np.uint16(np.around(points))[0, :]
 
         # create list of Balls from points
-        self.balls = [Ball((p[0], p[1])) for p in points]
+        ball_positions = [Ball((p[0], p[1])) for p in points]
+
+        to_identify = {}
+        identified = []
+
+        # identify balls - based on characteristics
+        for b in ball_positions:
+            roi, region = CV2Helper.roi(image, b.center, 10)
+
+            self.__correct_center(b, roi, region)
+
+            self.__mask_out_unimportant(b, roi, region)
+            self.__identify_ball(b, available_identities, to_identify, identified)
+        
+        # identify any leftover balls - likely one of the pairs is obstructed by view
+        for c, d in to_identify.items():
+            b = d[0]
+            if b.ratio > .15:
+                b.set_identity(available_identities[c].stripe)
+            else:
+                b.set_identity(available_identities[c].solid)
+
+            identified.append(b)
+
+        self.balls = identified
    
         self.updated.set()
+
+    # compounding error when hough circle is off
+    def __correct_center(self, b, image, region):
+        height, width, channels = image.shape
+
+        # create circular mask around approximated center
+        circle_mask = np.zeros((height,width), np.uint8)
+        cv2.circle(circle_mask, (b.center[0]-region[0], b.center[1]-region[1]), 10, [255, 255, 255], -1)
+        correction_mask = cv2.bitwise_and(image, image, mask=circle_mask)
+
+        # crop image from edge to first nonzero row/col
+        correction_cropped = np.nonzero(correction_mask) 
+        top = correction_cropped[0].min()
+        bottom = correction_cropped[0].max()
+        left = correction_cropped[1].min()
+        right = correction_cropped[1].max()
+        
+        # calculate corrected center based on minimums/maximums
+        w_correction = ((width-1) - (right - left)) // 2
+        y_correction = ((height-1) - (bottom - top)) // 2
+        y_correction = y_correction if top > 0 else -y_correction
+        w_correction = w_correction if left > 0 else -w_correction
+        corrected = b.center[0] + w_correction, b.center[1] + y_correction
+
+        b.update(corrected)
+
+    def __mask_out_unimportant(self, b, image, region):
+        # image_blur = cv2.GaussianBlur(image, (3, 3), 0)
+        height, width, channels = image.shape
+
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) 
+
+        # create roi smaller than ball to avoid color bias of balls in close proximity
+        blank_image = np.zeros((height,width), np.uint8)
+        cv2.circle(blank_image, (b.center[0]-region[0], b.center[1]-region[1]), 9, [255, 255, 255], -1)
+
+        # mask out white to avoid color bias and get more accurate mean
+        white_mask = cv2.inRange(hsv, *self.white_mask)
+
+        # dilate to ensure good white masking
+        mask_white_invert = cv2.dilate(white_mask, np.ones((3, 3), np.uint8))
+        mask_white_invert = cv2.bitwise_not(mask_white_invert)
+        mask_white_invert = cv2.bitwise_and(mask_white_invert, blank_image)
+
+        # mask out default dan pool stick
+        stick_mask = cv2.inRange(hsv, *self.stick_mask)
+        stick_mask_invert = cv2.dilate(stick_mask, np.ones((2, 2), np.uint8))
+        stick_mask_invert = cv2.bitwise_not(stick_mask_invert)
+
+        # in the case the player can move the cue ball, mask out glove
+        glove_mask = cv2.inRange(hsv, *self.glove_mask)
+        glove = cv2.dilate(glove_mask, np.ones((2, 2), np.uint8))
+        glove_mask = cv2.bitwise_not(glove_mask)
+
+        # combine masks and mask out undesired areas
+        mask = cv2.bitwise_and(mask_white_invert, glove_mask)
+        mask = cv2.bitwise_and(mask, stick_mask_invert)
+
+        mean_mask = cv2.bitwise_and(image, image, mask=mask)
+
+        b.ball_mask = mean_mask
+        b.white_mask = white_mask
+        b.stick_mask = stick_mask
+        b.glove_mask = glove_mask
+
+    def __identify_ball(self, b, available_identities, to_identify, identified):
+        b.get_features()
+
+        # get mean excluding black pixels (masked out)
+        color = b.ball_mask[~np.all(b.ball_mask == 0, axis=-1)].mean(axis=0)
+        color_hsv = cv2.cvtColor(np.uint8([[color]]), cv2.COLOR_BGR2HSV)[0][0]
+
+        # target identifier will return nan (most of the time)
+        if np.isnan(color).any():
+            identified.append(b)
+            return
+
+        # get list of colors - ordered by smallest deltae
+        color_proximities = CV2Helper.color_deltas(color, self.ball_color_look_up)
+        for dist, c in color_proximities:
+            # color should be in list of currently available targets
+            if c in available_identities:
+                color_info = constants.table.balls.colors.__dict__[c]
+
+                # verify color identifiction - hue should be within its mask range
+                if color_hsv[0] >= color_info.mask_lower[0] and color_hsv[0] <= color_info.mask_upper[0]:
+                    
+                    # set identity after identifying all balls of same color (except cue/eightball)
+                    if c in ["white", "black"]:
+                        if b.color_total > 17:
+                            b.set_identity(available_identities[c])
+                        identified.append(b)
+                        break
+                    else:
+                        total_identities = len(available_identities[c].__dict__)
+
+                        # skip identification if there should be another ball of the same color
+                        if c not in to_identify and total_identities > 1:
+                            to_identify[c] = [b]
+                        else:
+                            # identify with only available identity
+                            if total_identities == 1:
+                                identity = list(available_identities[c].__dict__.items())[0]
+                                b.set_identity(identity)
+                                identified.append(b)
+                                del to_identify[c]
+                            elif total_identities == 2:
+                                # approx suit by comparing ratios of white to colored pixels
+                                b1 = to_identify[c][0]
+                                
+                                if (b.ratio) > (b1.ratio):
+                                    b.set_identity(available_identities[c].stripe)
+                                    b1.set_identity(available_identities[c].solid)
+                                else:
+                                    b.set_identity(available_identities[c].solid)
+                                    b1.set_identity(available_identities[c].stripe)
+
+                                identified.append(b)
+                                identified.append(b1)
+                                del to_identify[c]
+                    break
 
     def draw(self, config):
         # draw on correct image type
